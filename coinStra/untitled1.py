f6fc8fcb58ace243d268e7b8789785efe8a3825a
zgoose <zzy361@hotmail.com>
2018-08-29 18:57:12 +0800

    modify untitle2.py

--- coinStra/untitled1.py
+++ coinStra/untitled1.py
@@ -1,256 +1,70 @@
-import json
-import requests
-from urllib import request ,parse
-import ssl
-import time
-from hashlib import sha1
-import random
-import string
-import hashlib
+import wx
+import wx.stc as stc
 
-class bitlim_api:
-    def __init__(self,api_key,api_secret):
-        self.api_key = api_key
-        self.api_secret = api_secret
-        self.url = "https://api.bitlim.com/api"
-  
-    def get_depth(self, market):    
-        resource = "%s/trade/trade/trade/trade?symbol=%s&brokerId=10003"%(self.url,market)
-        context = ssl._create_unverified_context()
-        req = request.Request(url=resource)
-        res = request.urlopen(req,context=context)
-        res = res.read().decode('utf-8')
-        res_dict = json.loads(res)
-        res = {}
-        res['bids'] = res_dict['buy']
-        res['asks'] = res_dict['sell']
-        return res
+class Mywin(wx.Frame): 
+   def __init__(self, parent, title): 
+      super(Mywin, self).__init__(parent, title = title,size = (350,250))
+		
+      panel = wx.Panel(self) 
+      vbox = wx.BoxSizer(wx.VERTICAL) 
+         
+      hbox1 = wx.BoxSizer(wx.HORIZONTAL) 
+      l1 = wx.StaticText(panel, -1, "文本域") 
+		
+      hbox1.Add(l1, 1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      self.t1 = wx.TextCtrl(panel) 
+		
+      hbox1.Add(self.t1,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      self.t1.Bind(wx.EVT_TEXT,self.OnKeyTyped) 
+      vbox.Add(hbox1) 
+		
+      hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+      l2 = wx.StaticText(panel, -1, "密码文本") 
+		
+      hbox2.Add(l2, 1, wx.ALIGN_LEFT|wx.ALL,5) 
+      self.t2 = wx.TextCtrl(panel)
 
-    def get_kline(self,market,n,typ):
-        ed = int(time.time())
-        be = ed - 24*60*60
-        resource = "%s/dataload/kline-query/pages?startTime=%s&endTime=%s&pageSize=%s&symbol=%s&kline=%s"%(self.url,be,ed,n,market,typ)
-        context = ssl._create_unverified_context()
-        req = request.Request(url=resource)
-        res = request.urlopen(req,context=context)
-        res = res.read().decode('utf-8')
-        res_dict = json.loads(res)
-        return res_dict
-#    def get_remain_order(self, market):
-#        apikey = self.apikey
-#        secretkey = self.secrete_key 
-#        
-#        resource = "%s/api/user/queryOrder"%self.url
-#        parms = {'market':market,'apikey':apikey}
-#        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-#
-#        context = ssl._create_unverified_context()
-#        req = request.Request(url=resource,data=textmod)
-#        res = request.urlopen(req,context=context)
-#        res = res.read().decode('utf-8')
-#        res_dict = json.loads(res)
-#        return res_dict
-    def _dict_sort_key(self,adict):
-        res = ''
-        for k in sorted(adict.values()):
-            res = res + k
-        return res
-
-    def _produceNonce(self,n):
-        return ''.join(random.sample(string.ascii_letters + string.digits, n))
-    
-    def _sha1(self,st):
-        psw=sha1()
-        psw.update(st.encode('utf8'))
-        return psw.hexdigest()
+      self.t2.SetMaxLength(5) 
+		
+      hbox2.Add(self.t2,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      vbox.Add(hbox2) 
+      self.t2.Bind(wx.EVT_TEXT_MAXLEN,self.OnMaxLen)
+		
+      hbox3 = wx.BoxSizer(wx.HORIZONTAL) 
+      l3 = wx.StaticText(panel, -1, "多行文本") 
+		
+      hbox3.Add(l3,1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      self.t3 = wx.TextCtrl(panel,size = (200,100),style = wx.TE_MULTILINE) 
+		
+      hbox3.Add(self.t3,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      vbox.Add(hbox3) 
+      self.t3.Bind(wx.EVT_TEXT_ENTER,self.OnEnterPressed)  
+		
+      hbox4 = wx.BoxSizer(wx.HORIZONTAL) 
+      l4 = wx.StaticText(panel, -1, "只读取文本") 
+		
+      hbox4.Add(l4, 1, wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      self.t4 = wx.TextCtrl(panel, value = "只读文本",style = wx.TE_READONLY|wx.TE_CENTER) 
+			
+      hbox4.Add(self.t4,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+      vbox.Add(hbox4) 
+      panel.SetSizer(vbox) 
         
-    def account_info(self):
-        parms = {'api_key':self.api_key,
-                 'nonce':self._produceNonce(18)}
-        st = self._dict_sort_key(parms)
-        parms['sign'] = self._sha1(st)
-
-        resource = "%s/usercoin/get_balance.do"%self.url
-        
-        req = requests.post(url=resource,data=json.dumps(parms))
-        print(req.text)
-        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-        context = ssl._create_unverified_context()
-        req = request.Request(url=resource,data=textmod)
-        res = request.urlopen(req,context=context)
-        res = res.read().decode('utf-8')
-        print(res)
-#        textmod = parms
-#        req = requests.post(url=resource,data=json.dumps(textmod))
-#        res_dict = json.loads(req.text)
-#        try:
-#            if res_dict['code'] == '100200':
-#                res = {}
-#                for i in range(len(res_dict['data'])):
-#                    dic = {'available':res_dict['data'][i]['amountAvailable'],'lock':res_dict['data'][i]['amountLock']}
-#                    res[res_dict['data'][i]['assetCode']] = dic
-#                return res
-#            else:
-#                print('winmax_account_info connect failed when time is ',ts)
-#        except:
-#            print('winmax_account_info connect failed when time is ',ts)
-    
-    def limit_order(self, market, price, num, side):
-        
-        ts = int(time.time())
-        ID = str(ts)+market+side+str(round(price,0)) + ''.join(random.sample(string.ascii_letters + string.digits, 4))
-        data = {'outOrderNo':ID,
-                'symbol':market,
-                'tradeCoinFlag':'FIXED',
-                'tradeCoinType':side,
-                'price':price,
-                'amount':num}
-        parms = {'api_key':self.api_key,
-                 'nonceStr':'1'*32,
-                 'timestamp':ts,
-                 'data':data,
-                 'sign':''}
-        
-        signal = {'api_secret':self.api_secret,
-                 'nonceStr':'1'*32,
-                 'timestamp':ts}
-        signal_url = parse.urlencode(self._dict_sort_key(dict(signal,**data)))
-        sign = hashlib.md5()
-        sign.update(signal_url.encode(encoding='utf-8'))
-        parms['sign'] = sign.hexdigest().upper()
-    
-        resource = "%s/exchangeApi/api/matchOrder"%self.url
-        
-#        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-
-#        context = ssl._create_unverified_context()
-        req = requests.post(url=resource,data=json.dumps(parms))
-#        res = request.urlopen(req,context=context)
-#        res = res.read().decode('utf-8')
-#        res_dict = json.loads(res)
-        res_dict = json.loads(req.text)
-        return ID
-
-    def order_cancel(self, order_id):
-        ts = int(time.time())
-        
-        data = {'outTradeNo':order_id}
-        parms = {'api_key':self.api_key,
-                 'nonceStr':'1'*32,
-                 'timestamp':ts,
-                 'data':data,
-                 'sign':''}
-        
-        signal = {'api_secret':self.api_secret,
-                 'nonceStr':'1'*32,
-                 'timestamp':ts}
-        signal_url = parse.urlencode(self._dict_sort_key(dict(signal,**data)))
-        sign = hashlib.md5()
-        sign.update(signal_url.encode(encoding='utf-8'))
-        parms['sign'] = sign.hexdigest().upper()
-    
-        resource = "%s/exchangeApi/api/cancel"%self.url
-        
-#        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-
-#        context = ssl._create_unverified_context()
-        req = requests.post(url=resource,data=json.dumps(parms))
-#        res = request.urlopen(req,context=context)
-#        res = res.read().decode('utf-8')
-#        res_dict = json.loads(res)
-        res_dict = json.loads(req.text)
-        return res_dict
-    
-    def check_remian_order(self, market):
-        ts = int(time.time())
-        
-        data = {'symbol':market}
-        parms = {'api_key':self.api_key,
-                 'nonceStr':'1'*32,
-                 'timestamp':ts,
-                 'data':data,
-                 'sign':''}
-        
-        signal = {'api_secret':self.api_secret,
-                 'nonceStr':'1'*32,
-                 'timestamp':ts}
-        signal_url = parse.urlencode(self._dict_sort_key(dict(signal,**data)))
-        sign = hashlib.md5()
-        sign.update(signal_url.encode(encoding='utf-8'))
-        parms['sign'] = sign.hexdigest().upper()
-    
-        resource = "%s/exchangeApi/api/matchOrder/process"%self.url
-        
-#        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-
-#        context = ssl._create_unverified_context()
-        req = requests.post(url=resource,data=json.dumps(parms))
-#        res = request.urlopen(req,context=context)
-#        res = res.read().decode('utf-8')
-#        res_dict = json.loads(res)
-        res_dict = json.loads(req.text)
-        try:
-            if res_dict['code'] == '100200':
-                res = res_dict['data']
-                if res == []:
-                    return [{'type':None,'price':None,'id':None,'amount':None}]
-                for i in range(len(res_dict['data'])):
-#                    dic = {'available':res_dict['data'][i]['amountAvailable'],'lock':res_dict['data'][i]['amountLock']}
-                    res[i]['amount'] = res[i]['numberOver']
-                    res[i]['type'] = res[i]['tradeCoinType']
-                    res[i]['id'] = res[i]['orderNo']
-                return res
-            else:
-                print('check_remian_order connect failed when time is ',ts)
-        except:
-            print('check_remian_order connect failed when time is ',ts)
-'''     
-    def get_trade_hist(self, market):
-        apikey = self.apikey
-        secretkey = self.secrete_key 
-        
-        resource = "%s/api/user/getMyTradeLog"%self.url
-        parms = {'market':market,
-                'apikey':apikey}
-        
-        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-
-        context = ssl._create_unverified_context()
-        req = request.Request(url=resource,data=textmod)
-        res = request.urlopen(req,context=context)
-        res = res.read().decode('utf-8')
-        res_dict = json.loads(res)
-        
-        return res_dict
-
-
-    def get_trade_ticker(self, market):
-        
-        resource = "%s/api/ticker/getTicker"%self.url
-        parms = {'market':market}
-        
-        textmod = parse.urlencode(parms).encode(encoding='utf-8')
-
-        context = ssl._create_unverified_context()
-        req = request.Request(url=resource,data=textmod)
-        res = request.urlopen(req,context=context)
-        res = res.read().decode('utf-8')
-        res_dict = json.loads(res)
-        
-        return res_dict
-'''
-        
-if __name__=='__main__':
-    wkt_t = bitlim_api('JKDSA21Ssdfdfs12OP','dfasd121kjfksldKJKF12FDSFSDFS')
-#    ss = wkt_t.account_info()
-#    ss = wkt_t.get_winmax_depth('UDC_ETH')
-#    wkt_t.limit_order('UDC_ETH', 502, 1, 'BUY')
-#    ss = wkt_t.order_cancel('a23')
-#    
-    wkt_t.account_info()
-#    print(ss)
-        
-
-
-
-
+      self.Centre() 
+      self.Show() 
+      self.Fit()  
+		
+   def OnKeyTyped(self, event): 
+      print(event.GetString())
+      stc.StyledTextCtrl(self.t2,style = wx.TE_PASSWORD)
+		
+   def OnEnterPressed(self,event): 
+      print("Enter pressed")
+      
+		
+   def OnMaxLen(self,event): 
+      print("Maximum length reached")
+		
+app = wx.App() 
+Mywin(None,  'TextCtrl实例-www.yiibai.com')
+app.MainLoop()
--- coinStra/untitled2.py
+++ coinStra/untitled2.py
@@ -1,12 +1,7 @@
 import wx
-import winmax_api_v02 as wmapi
 import time
-from CoinStrategy import *
 
-
-wmex = wmapi.winmax_api("","")        
-        
-class  PanelTwo(wx.Panel):
+class  stragety1(wx.Panel):
     def __init__(self, parent,sendsignal):
         self.themeColor = (200,0,0)
         enter_font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
@@ -16,7 +11,7 @@ class  PanelTwo(wx.Panel):
         wx.Panel.__init__(self, parent)
         
         # A multiline TextCtrl - This is here to show how the events work in this program, don't pay too much attention to it
-        #self.logger = wx.TextCtrl(self, pos=(300, 20), size=(470, 300), style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.logger = wx.TextCtrl(self, pos=(300, 20), size=(470, 300), style=wx.TE_MULTILINE | wx.TE_READONLY)
 
         # A button
         self.button1 = wx.Button(self, label=u"开始运行", pos=(60, 280))
@@ -131,7 +126,129 @@ class  PanelTwo(wx.Panel):
         except:
             wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
 
-class PanelOne(wx.Panel):
+class  chooseStragety(wx.Panel):
+    def __init__(self, parent,sendsignal):
+        self.themeColor = (200,0,0)
+        enter_font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        font = wx.Font(14, wx.ROMAN,wx.ITALIC,wx.FONTWEIGHT_BOLD)
+        self.para = [0,0,0,0,0]
+        self.sendsignal = sendsignal
+        wx.Panel.__init__(self, parent)
+
+        # A button
+        self.button1 = wx.Button(self, label=u"开始运行", pos=(60, 280))
+        self.Bind(wx.EVT_BUTTON, self.raiseStra, self.button1)
+        self.button1 .SetForegroundColour('white')
+        self.button1 .SetBackgroundColour(self.themeColor)
+        
+        
+        self.button2 = wx.Button(self, label=u"停止运行", pos=(180, 280))
+        self.Bind(wx.EVT_BUTTON, self.stopStra, self.button2)
+        self.button2 .SetForegroundColour('white')
+        self.button2 .SetBackgroundColour(self.themeColor)
+
+        self.lblhear = wx.StaticText(self, label=u"选择币种：", pos=(20, 20))
+        self.lblhear.SetForegroundColour(self.themeColor)
+        self.lblhear.SetFont(font)
+        
+        self.sampleList = [u'BTC', u'ETH',u'WKT']
+        self.edithear = wx.ComboBox(self, pos=(150, 20), size=(95, -1), choices=self.sampleList, style=wx.CB_DROPDOWN)
+        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.edithear)
+        
+        
+        self.lblname1 = wx.StaticText(self, label=u"入场点:", pos=(20, 60))
+        self.lblname1.SetForegroundColour(self.themeColor)
+        self.lblname1.SetFont(font)
+        
+        self.editname1 = wx.TextCtrl(self, pos=(150, 60), size=(140, -1))
+        self.editname1.SetForegroundColour('gray')
+        self.editname1.SetFont(enter_font)
+        self.Bind(wx.EVT_TEXT, self.EvtText1, self.editname1)
+
+        self.lblname2 = wx.StaticText(self, label=u"出场点:", pos=(20, 100))
+        self.lblname2.SetForegroundColour(self.themeColor)
+        self.lblname2.SetFont(font)
+        self.editname2 = wx.TextCtrl(self, pos=(150, 100), size=(140, -1))
+        self.editname2.SetForegroundColour('gray')
+        self.editname2.SetFont(enter_font)
+        self.Bind(wx.EVT_TEXT, self.EvtText2, self.editname2)
+        
+        self.lblname3 = wx.StaticText(self, label=u"单笔交易量:", pos=(20, 140))
+        self.lblname3.SetForegroundColour(self.themeColor)
+        self.lblname3.SetFont(font)
+        self.editname3 = wx.TextCtrl(self, pos=(150, 140), size=(140, -1))
+        self.editname3.SetForegroundColour('gray')
+        self.editname3.SetFont(enter_font)
+        self.Bind(wx.EVT_TEXT, self.EvtText3, self.editname3)
+        
+        self.lblname4 = wx.StaticText(self, label=u"止损(%):", pos=(20, 180))
+        self.lblname4.SetForegroundColour(self.themeColor)
+        self.lblname4.SetFont(font)
+        self.editname4 = wx.TextCtrl(self, pos=(150, 180), size=(140, -1))
+        self.editname4.SetForegroundColour('gray')
+        self.editname4.SetFont(enter_font)
+        self.Bind(wx.EVT_TEXT, self.EvtText4, self.editname4)
+        
+        self.lblname5 = wx.StaticText(self, label=u"止赢(%):", pos=(20, 220))
+        self.lblname5.SetForegroundColour(self.themeColor)
+        self.lblname5.SetFont(font)
+        self.editname5 = wx.TextCtrl(self, pos=(150, 220), size=(140, -1))
+        self.editname5.SetForegroundColour('gray')
+        self.editname5.SetFont(enter_font)
+        self.Bind(wx.EVT_TEXT, self.EvtText5, self.editname5)
+    
+    def EvtText1(self, event):
+        if float(event.GetString()) <= 50 and float(event.GetString()) >= 1:
+            self.para[0] = float(event.GetString())
+        else:
+            wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
+            
+    def EvtText2(self, event):
+        if float(event.GetString()) <= 100:
+            self.para[1] = float(event.GetString())
+        else:
+            wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
+            
+    def EvtText3(self, event):
+        if float(event.GetString()) <= 50 and float(event.GetString()) >= 1:
+            self.para[2] = float(event.GetString())
+        else:
+            wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
+            
+    def EvtText4(self, event):
+        if float(event.GetString()) <= 1:
+            self.para[3] = float(event.GetString())
+        else:
+            wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
+    
+    def EvtText5(self, event):
+        if float(event.GetString()) <= 1:
+            self.para[4] = float(event.GetString())
+        else:
+            wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
+    
+    def EvtComboBox(self, event):
+        self.coin_name = 'ETH'
+        if event.GetString() == u'BTC':
+            self.coin_name = 'BTC'
+        elif event.GetString() == u'ETH':
+            self.coin_name = 'ETH'
+        elif event.GetString() == u'WKT':
+            self.coin_name = 'WKT'
+
+    def raiseStra(self, event):
+        print(self.coin_name,self.para,wmex,self.logger,self.sendsignal)
+        self.stra = CoinStrategy(self.coin_name,self.para,wmex,self.logger,self.sendsignal)
+        self.stra.start()
+
+    def stopStra(self, event):
+        try:
+            self.stra.stop()
+            del self.stra
+        except:
+            wx.MessageBox(u"输入错误", "Error" ,wx.OK | wx.ICON_ERROR)
+
+class Login(wx.Panel):
     def __init__(self, parent,function):
         self.parent = parent
         self.function = function
@@ -166,52 +283,66 @@ class PanelOne(wx.Panel):
 
 
     def sureEvent(self, event):
-        global wmex
         account = self.accountInput.GetValue()
         password = self.passwordInput.GetValue()
-        wmex = wmapi.winmax_api(account,password)
-        info = wmex.account_info()
-        if info == None:
-            wx.MessageBox(u"密码错误，或帐号不存在", "Error" ,wx.OK | wx.ICON_ERROR)
+        if account == 'test' and password == 'test':
+        	self.function(event)
         else:
-            self.function(event)
+            wx.MessageBox(u"密码错误，或帐号不存在", "Error" ,wx.OK | wx.ICON_ERROR)
 
 
-########################################################################
 class MyForm(wx.Frame):
-
-    #----------------------------------------------------------------------
     def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY,u'数字货币自动交易工具',size=(800,400))
+        wx.Frame.__init__(self, None, wx.ID_ANY,'trade tool',size=(800,400))
         
-        self.panel_one = PanelOne(self,self.onSwitchPanels)
-        self.panel_two = PanelTwo(self,self.signal)
-        self.panel_two.Hide()
+        self.panel_login = Login(self,self.onReturnToChooseStragety)
+        self.panel_chooseStrategy = chooseStragety(self,self.onSwitchStragety)
+        self.panel_StragetyOne = stragety1(self,self.onReturnToChooseStragety,self.signal)
+        self.panel_StragetyTwo = stragety2(self,self.onReturnToChooseStragety,self.signal)
+
+        self.panel_chooseStrategy.Hide()
+        self.panel_StragetyOne.Hide()
+        self.panel_StragetyTwo.Hide()
         
-                # the combobox Control
+# 		 the combobox Control
         self.statusbar = self.CreateStatusBar()
-#        #将状态栏分割为3个区域,比例为1:2:3
+#        将状态栏分割为3个区域,比例为1:2:3
 #        self.statusbar.SetFieldsCount(3)
 #        self.statusbar.SetStatusWidths([-1, -2, -3])
         
         self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(self.panel_one, 1, wx.EXPAND)
-        self.sizer.Add(self.panel_two, 1, wx.EXPAND)
+        self.sizer.Add(self.panel_login, 1, wx.EXPAND)
+        self.sizer.Add(self.panel_chooseStrategy, 1, wx.EXPAND)
+        self.sizer.Add(self.panel_StragetyOne, 1, wx.EXPAND)
+        self.sizer.Add(self.panel_StragetyTwo, 1, wx.EXPAND)
+
         self.SetSizer(self.sizer)
 
     #----------------------------------------------------------------------
-    def onSwitchPanels(self, event):
-        """"""
-        if self.panel_one.IsShown():
-            self.SetTitle(u"数字货币自动交易工具")
-            self.panel_one.Hide()
-            self.panel_two.Show()
+    def onReturnToChooseStragety(self, event):
+        self.SetTitle('Choose_Stragety')
+        if self.panel_login.IsShown():
+            self.panel_login.Hide()
+        elif self.panel_StragetyOne.IsShown():
+        	self.panel_StragetyOne.Hide()
+        elif self.panel_StragetyTwo.IsShown():
+        	self.panel_StragetyTwo.Hide()
+            
+        self.panel_chooseStrategy.Show()
         self.Layout()
-        
+
+    def onSwitchStragety(self,event,stragetyNo):
+    	self.panel_chooseStrategy.Hide()
+    	if stragetyNo == 1:
+    		self.panel_StragetyOne.Show()
+    	elif stragetyNo == 2:
+    		self.panel_StragetyTwo.Show()
+    	self.Layout()
+
     def signal(self):
         t = time.localtime(time.time()) 
         StrYMDt = time.strftime("%Y-%m-%d %H:%M", t) 
-        self.SetStatusText(u'程序最新运行时间：'+StrYMDt,0)
+        self.SetStatusText('last running time is '+StrYMDt,0)
 
 # Run the program
 if __name__ == "__main__":
